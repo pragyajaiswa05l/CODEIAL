81c79286bf8431c79637e213b8b0d106bda41da4
Pragya <prag05jaiswal@gmail.com>
2021-01-02 21:19:28 +0530

    first commit: create the directory structure and setup express server

---
+++ index.js
@@ -0,0 +1,15 @@
+// 1) Import the express module
+const express = require('express');
+const app = express();
+
+// 2) defining port no.
+const port = 8000;
+
+// 3) Running the server on defined port
+app.listen(port,function(err){
+    if(err){
+        console.log(`Error in running the server :${err}`);
+        return;
+    }
+    console.log(`Server is running  on port : ${port}`);
+})
