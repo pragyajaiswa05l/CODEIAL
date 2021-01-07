ff6acf26bc64e0a74e3b8924989ea1edeefc0e47
Pragya <prag05jaiswal@gmail.com>
2021-01-07 19:54:49 +0530

    rendering pages for sign u and sign in

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,6 +1,24 @@
+//render the user profile page
+
 module.exports.profile = function(req,res){
     //res.end('<h1>User Profile</h1>');
     return res.render('user_profile',{
-        title:"user Profile"
+        title:"User Profile"
+    });
+}
+
+
+//render the sign up page
+module.exports.signUp = function(req,res){
+    return res.render('user_sign_up',{
+        title: "Codeial | Sign Up"
+    });
+}
+
+
+//render the sign in page
+module.exports.signIn = function(req,res){
+    return res.render('user_sign_in',{
+        title: "Codeial | Sign In"
     });
 }
