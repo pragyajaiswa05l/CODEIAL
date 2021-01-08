b0deb7ccccba525acac92c219b8d42c10b587a75
Pragya <prag05jaiswal@gmail.com>
2021-01-08 22:36:32 +0530

    user sign up

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,7 @@
+//to use the schema
+const User = require('../models/user');
+
+
 //render the user profile page
 
 module.exports.profile = function(req,res){
@@ -25,7 +29,34 @@ module.exports.signIn = function(req,res){
 
 //get the sign up data
 module.exports.create = function(req,res){
-    // TODO LATER
+
+    //check if password and confirm password are same or not 
+    if (req.body.password != req.body.confirm_password){
+        return res.redirect('back'); //go the same page it came from
+    }
+
+    //if the user doesn't exit create it
+    User.findOne({email: req.body.email},function(err,user){
+        if(err){
+            console.log('error in finding user in signing up');
+            return;
+        }
+
+        if(!user){
+            User.create(req.body , function(err,user){
+                if(err){
+                    console.log('error in creating user while signing up');
+                    return;
+                } 
+                return res.redirect('/users/sign-in'); 
+            })
+        }
+
+        //if user already exist
+        else{
+            return res.redirect('back');
+        }
+    });
 }
 
 
