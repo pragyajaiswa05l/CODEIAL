2982f8916db7e9bb201a1f493215564f9d4aa7c1
Pragya <prag05jaiswal@gmail.com>
2021-01-30 12:02:01 +0530

    setting up scss

--- index.js
+++ index.js
@@ -29,6 +29,19 @@ app.use(expressLayouts);
 
  const MongoStore = require('connect-mongo')(session);
 
+ const sassMiddleware = require('node-sass-middleware');
+
+
+
+
+ app.use(sassMiddleware({
+     src: '/assets/scss',
+     dest: '/assets/css',
+     debug: true,
+     outputStyle: 'extended',
+     prefix: '/css'
+ }));
+
  //reading through the post request
  app.use(express.urlencoded());
 
