0379a1c3cc6252dce0e420291251bce162aa238f
Pragya <prag05jaiswal@gmail.com>
2021-02-01 12:48:28 +0530

    saving posts to the database

--- routes/index.js
+++ routes/index.js
@@ -14,7 +14,7 @@ router.get('/', homeController.home);
 //for any further routes access from here
 //router.use('/routerName',require('./routerfile'));
 router.use('/users',require('./users'));
-router.use('/users',require('./posts'));
+router.use('/posts',require('./posts'));
 
 
 module.exports = router;
