f78f88181759fea625467740e73ca218b8685b67
Pragya <prag05jaiswal@gmail.com>
2021-02-02 12:52:31 +0530

    display posts and related users

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,11 +1,32 @@
+const Post = require('../models/post');
+
+
+
 module.exports.home = function(req, res){
     // return res.end('<h1>Express is up for Codeial!</h1>');
 
-    console.log(req.cookies);
-    res.cookie('user_id',25);
+    //console.log(req.cookies);
+   // res.cookie('user_id',25);
+
+   //this query will return all the post
+//    Post.find({},function(err,posts){
+//         return res.render('home',{
+//             title:"Codeial | Home",
+//             posts: posts
+//         });
+//    });
+
+
+//populate the user of each post
+Post.find({}).populate('user').exec(function(err,posts){
     return res.render('home',{
-        title:"Home"
+        title:"Codeial | Home",
+        posts: posts
     });
+});
+
+
+    
 }
 
 // module.exports.actionName = function(req, res){}
--- models/post.js
+++ models/post.js
@@ -12,7 +12,7 @@ const postSchema = new mongoose.Schema({
         ref: 'User'
     }
 },{
-        timestamps:true 
+    timestamps:true 
     
 });
 
