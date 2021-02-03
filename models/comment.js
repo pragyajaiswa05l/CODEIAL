73498d2be4355f9f4c7f72e19ad86b4e58b8f1c3
Pragya <prag05jaiswal@gmail.com>
2021-02-03 09:52:02 +0530

    creating schema for comments

---
+++ models/comment.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+
+
+const commentSchema = new mongoose.Schema({
+    content:{
+        type: String,
+        required: true
+    },
+    //comment belongs to a user
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User'
+    },
+    post: {
+        type:mongoose.Schema.Types.ObjectId,
+        ref: 'Post'
+    }
+},{
+    timestamps: true
+});
+
+//we need to tell mongoose that this is going to be a collection
+const Comment = mongoose.model('Comment',commentSchema);
+module.exports = Comment;
