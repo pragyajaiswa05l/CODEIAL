aecb75489d320c483d77e03cc67c99ee742becd1
Pragya <prag05jaiswal@gmail.com>
2021-02-09 20:56:45 +0530

    deleting a post authorised

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,5 @@
 const Post = require('../models/post');
+const Comment = require('../models/comment');
 
 module.exports.create = function(req , res){
     Post.create({
@@ -10,5 +11,29 @@ module.exports.create = function(req , res){
             return;
         }
         return res.redirect('back');
+    });
+}
+
+module.exports.destroy = function(req,res){
+    //find whether the post exist or not before deleting it
+    Post.findById(req.params.id, function(err,post){
+        //.id means converting the object id into string
+        //if post exist
+        //check whether the user who is deleting the post is the same user who has written the post
+        if(post.user == req.user.id){
+            //delete the post and the comment also from that post
+            post.remove();
+
+            Comment.deleteMany({post:req.params.id},function(err){
+                return res.redirect('back');
+            });
+
+
+    
+        }
+        //if user didn't match
+        else{
+            return res.redirect('back');
+        }
     });
 }
