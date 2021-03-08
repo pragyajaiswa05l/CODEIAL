9ec5f71565ebcdc9bc5755d208573c6fbb6ba594
Pragya <prag05jaiswal@gmail.com>
2021-03-08 11:55:56 +0530

    installing multer and importing it

--- models/user.js
+++ models/user.js
@@ -1,5 +1,13 @@
 const mongoose = require('mongoose');
 
+
+//importing multer
+const multer = require('multer');
+//requiring path(setting the path where file will be stored)
+const path = require('path');
+const AVATAR_PATH = path.join('/uploads/users/avatars');
+
+
 // to create a schema
 const userSchema = new mongoose.Schema({
     email:{
